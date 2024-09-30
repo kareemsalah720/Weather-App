60ca7f6047aef70d5ab9d3f928ca1a8d7edbbbda
kareemsalah720@gmail.com <kareemsalah720@gmail.com>
2024-09-30 08:45:54 +0300

    create weather services

--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -1,3 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:weather_app/model/wearther_model.dart';
+
 class WeatherService {
-  
+  Future<List<WeatherModel>> getWeather({required String cityName}) async {
+    try {
+      Response response = await Dio().get(
+          'http://api.weatherapi.com/v1/forecast.json?key=a42cf598a5274a1497b64737240305&q=$cityName&days=3&aqi=no&alerts=no');
+
+      List<WeatherModel> weatherList = [
+        WeatherModel.fromJson(response.data, 0),
+        WeatherModel.fromJson(response.data, 1),
+        WeatherModel.fromJson(response.data, 2),
+      ];
+      return weatherList;
+    } on DioException catch (e) {
+      final errorMessage =
+          e.response?.data['error']['message'] ?? 'Ooops Something went wrong';
+      throw Exception(errorMessage);
+    } catch (e) {
+      throw Exception('try again $e');
+    }
+  }
 }
